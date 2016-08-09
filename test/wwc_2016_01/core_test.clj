f6b18abc68b3f877560afd976d3816e80003afb7
Frederico Lellis de Albuquerque <frederico@molequedeideias.net>
2016-08-09 20:25:55 -0300

    1o

--- test/wwc_2016_01/core_test.clj
+++ test/wwc_2016_01/core_test.clj
@@ -4,15 +4,13 @@
             [wwc-2016-01.core :refer :all]))
 
 
-;(defn solucao-min [max array]
-;  (first (sorted-set-by (fn [x y]
-;                          (> (reduce + x) (reduce + y)))
-;                        (filter (fn
-;                                  [item]
-;                                  (< (reduce + item) max)) (clojure.math.combinatorics/subsets array)))))
+(defn wwwc-01-filter [max array]
+  (first (apply sorted-set-by (fn [x y] (> (reduce + x) (reduce + y)))
+                (filter (fn [item] (< (reduce + item) max))
+                        (clojure.math.combinatorics/subsets array)))))
 
 
-(defn wwwc-01 [x array]
+(defn wwwc-01-loop [x array]
   (loop
     [combinacoes (clojure.math.combinatorics/subsets array)
      array-maior-menores []]
@@ -24,6 +22,10 @@
 
 (deftest a-test
   (testing "Maior menor"
-    (is (= (wwwc-01 29 [2, 8, 3, 9, 11]) [8 9 11]))))
+    (is (= (wwwc-01-loop 29 [2, 8, 3, 9, 11]) [8 9 11]))
+    (is (= (wwwc-01-filter 29 [2, 8, 3, 9, 11]) [8 9 11]))))
 
 
+(do
+  (time (wwwc-01-filter 29 [2, 8, 3, 9, 11]))
+  (time (wwwc-01-loop 29 [2, 8, 3, 9, 11])))
