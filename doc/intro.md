2f80ed83aa0df89d42dc1bd2efe12812de2f1067
Frederico Lellis de Albuquerque <frederico@molequedeideias.net>
2016-08-09 19:17:29 -0300

    1o

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to wwc-2016-01
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/wwc_2016_01/core_test.clj
@@ -0,0 +1,34 @@
+(ns wwc-2016-01.core-test
+  (:require [clojure.test :refer :all]
+            [clojure.math.combinatorics :as combo]
+            [wwc-2016-01.core :refer :all]))
+
+
+(def vector-of-vectors [[1 2 3] [] [1] []])
+
+(def populated-vector?
+  )
+
+(defn solucao-fred [max array]
+  (first (sorted-set-by (fn [x y]
+                          (> (reduce + x) (reduce + y)))
+                        (filter (fn
+                                  [item]
+                                  (< (reduce + item) max)) (clojure.math.combinatorics/subsets array)))))
+
+
+(defn wwwc-01 [x array]
+  (loop
+    [combinacoes (clojure.math.combinatorics/subsets array)
+     array-maior-menores []]
+    (if (empty? combinacoes)
+      (vec array-maior-menores)
+      (recur (rest combinacoes) (if (and (< (reduce + (first combinacoes)) x) (> (reduce + (first combinacoes)) (reduce + array-maior-menores)))
+                                  (first combinacoes)
+                                  array-maior-menores)))))
+
+(deftest a-test
+  (testing "Maior menor"
+    (is (= (wwwc-01 29 [2, 8, 3, 9, 11]) [8 9 11]))))
+
+
