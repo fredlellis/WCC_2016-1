f408aa95332893a3ef505b8a7cdc7b12615e80a9
Frederico Lellis de Albuquerque <frederico@molequedeideias.net>
2016-08-09 22:49:27 -0300

    1o

--- test/wwc_2016_01/core_test.clj
+++ test/wwc_2016_01/core_test.clj
@@ -1,18 +1,19 @@
 (ns wwc-2016-01.core-test
   (:require [clojure.test :refer :all]
-            [clojure.math.combinatorics :as combo]
-            [wwc-2016-01.core :refer :all]))
+            [clojure.math.combinatorics :refer :all]))
 
+
+(defn m [x y](->>(subsets y)(filter(fn[i](<(reduce + i)x)))(apply sorted-set-by #(>(reduce + %1)(reduce + %2)))(first)))
 
 (defn wwwc-01-filter [max array]
   (first (apply sorted-set-by (fn [x y] (> (reduce + x) (reduce + y)))
                 (filter (fn [item] (< (reduce + item) max))
-                        (clojure.math.combinatorics/subsets array)))))
+                        (subsets array)))))
 
 
 (defn wwwc-01-loop [x array]
   (loop
-    [combinacoes (clojure.math.combinatorics/subsets array)
+    [combinacoes (subsets array)
      array-maior-menores []]
     (if (empty? combinacoes)
       (vec array-maior-menores)
@@ -23,9 +24,13 @@
 (deftest a-test
   (testing "Maior menor"
     (is (= (wwwc-01-loop 29 [2, 8, 3, 9, 11]) [8 9 11]))
-    (is (= (wwwc-01-filter 29 [2, 8, 3, 9, 11]) [8 9 11]))))
+    (is (= (wwwc-01-filter 29 [2, 8, 3, 9, 11]) [8 9 11]))
+    (is (= (wwwc-01-filter 29 [2, 8, 3, 9, 11, -4]) [8 9 11]))
+    (is (= (wwwc-01-filter 31 [2, 8, 3, 9, 11, -4, 34]) [-4 34]))
+    (is (= (m 31 [2, 8, 3, 9, 11, -4, 34]) [-4 34]))))
 
 
 (do
   (time (wwwc-01-filter 29 [2, 8, 3, 9, 11]))
-  (time (wwwc-01-loop 29 [2, 8, 3, 9, 11])))
+  (time (wwwc-01-loop 29 [2, 8, 3, 9, 11]))
+  (time (m 29 [2, 8, 3, 9, 11])))
